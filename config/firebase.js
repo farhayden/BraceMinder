f75ccb12c3438c883f8cd8dfe560b7f38f0cb66f
farhayden <haydefa1@student.op.ac.nz>
2023-09-08 10:21:41 +1200

    Create(new folder & file): add new folder to containn firebase configurations from SDK snippet from firebase setup

---
+++ config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBgOAiBhEKMxZ5C5IwnbWolP4b6g6EimXc",
+  authDomain: "braceminder.firebaseapp.com",
+  projectId: "braceminder",
+  storageBucket: "braceminder.appspot.com",
+  messagingSenderId: "588300056336",
+  appId: "1:588300056336:web:701418594ed31a58495795"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
