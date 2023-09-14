47403f3ec0733b6d356264ad7b1de079e66294f3
farhayden <haydefa1@student.op.ac.nz>
2023-09-14 15:32:07 +1200

    Modify(entire file): add navigation functionality the same as in our other repo

---
+++ services/Navigation.jsx
@@ -0,0 +1,84 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+
+import HomeScreen from "../screens/HomeScreen";
+import Reminders from "../screens/Reminders";
+import SetReminder from "../screens/SetReminder";
+
+const HomeStack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HOMESCREENICON = "home";
+const REMINDERSCREENICON = "alarm";
+
+function HomeStackScreen({ initialScreen }) {
+    return (
+      <HomeStack.Navigator
+        initialRouteName={initialScreen}
+        screenOptions={defaultScreenOptions}
+      >
+        <HomeStack.Screen name="BraceMinder" component={HomeScreen} />
+        <HomeStack.Screen name="Reminders" component={Reminders} />
+      </HomeStack.Navigator>
+    );
+}
+
+function BottomTabNav() {
+    return (
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen
+            name="BraceMinder"
+            options={{
+              headerShown: false,
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons
+                  name={HOMESCREENICON}
+                  color={color}
+                  size={size}
+                />
+              ),
+            }}
+          >
+            {(props) => (
+              <HomeStackScreen {...props} initialScreen="BraceMinder" />
+            )}
+          </Tab.Screen>
+  
+          <Tab.Screen
+            name="My Reminders"
+            options={{
+              headerShown: false,
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons
+                  name={REMINDERSCREENICON}
+                  color={color}
+                  size={size}
+                />
+              ),
+            }}
+          >
+            {(props) => <HomeStackScreen {...props} initialScreen="Reminders" />}
+          </Tab.Screen>
+        </Tab.Navigator>
+      </NavigationContainer>
+    );
+  }
+  
+  const defaultScreenOptions = {
+    headerStyle: {
+      backgroundColor: "rgb(65, 115, 222)",
+    },
+    headerTintColor: "white",
+    headerTitleAlign: 'center',  
+    headerTitleStyle: {
+      fontWeight: "bold",
+      flexGrow: 1,
+  
+    },
+  };
+  
+  export { BottomTabNav };
