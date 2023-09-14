f3a7457dd13b255f308e55d09ba88a99d1b6296f
farhayden <haydefa1@student.op.ac.nz>
2023-09-14 15:30:40 +1200

    Modify(function & return): add a function to index reminders; add return to render list of reminders on the screen

--- screens/Reminders.jsx
+++ screens/Reminders.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { View, Text, TouchableOpacity, Button } from "react-native";
 import scheduleLocalNotification from "../services/RemindersService";
 import SetReminder from "./SetReminder";
 
@@ -8,4 +9,24 @@ function App() {
         { title: 'Rubber Bands', message: '', subtext: '', bigText: '', time: '12:00', repeatTime: 1 },
         // Add more initial reminders as needed
     ]);
-}
+
+    // Function to schedule a reminder
+    const handleScheduleReminder = (reminderIndex) => {
+        const reminderToSchedule = reminders[reminderIndex];
+        // Call the scheduleLocalNotification function with the reminder data
+        scheduleLocalNotification(reminderToSchedule);
+    };
+
+    return (
+        <View>
+            {/* Render a list of reminders and pass the scheduling function */}
+            {reminders.map((reminder, index) => (
+                <SetReminder
+                    key={index}
+                    reminder={reminder}
+                    onSchedule={() => handleScheduleReminder(index)}
+                />
+            ))}
+        </View>
+    );
+} export default App;
