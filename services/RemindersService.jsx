d750926592ba6d3397e08afd55b90944edf914b1
farhayden <haydefa1@student.op.ac.nz>
2023-09-18 14:31:00 +1200

    Modify(values): alter some of the notification property values

--- services/RemindersService.jsx
+++ services/RemindersService.jsx
@@ -6,12 +6,12 @@ function scheduleLocalNotification() {
         channelId: "BraceMinder-channel-id",
         title: "Tooth Brushing", // (optional)
         message: "My Notification Message", // (required)
-        color: "red",
+        color: "blue",
         vibrate: true,
         vibration: 300,
         subtext: "This is a subtext",
-        bigText: "This is a place holder for tooth brushing reminder",
-        date: selectedTime,
+        bigText: "",
+        date: new Date,
         repeatTime: 'daily',
     });
 } export default scheduleLocalNotification;
