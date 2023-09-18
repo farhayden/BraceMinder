916710b28a22cb10574b06c2585d8cd53b97b2e5
farhayden <haydefa1@student.op.ac.nz>
2023-09-18 14:27:45 +1200

    Modify(datetimepicker, styling): add datetimepicker element for evening reminder; add styling to screen elements

--- screens/ToothBrushing.jsx
+++ screens/ToothBrushing.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, TextInput } from "react-native";
+import { View, Text, TouchableOpacity, TextInput, Image, StyleSheet } from "react-native";
 import DateTimePicker from '@react-native-community/datetimepicker'; // Import the datetimepicker component
 import scheduleLocalNotification from "../services/RemindersService";
+import logo from "../assets/logo.png";
+
+const LOGO = logo;
 
 function ToothBrushing() {
     const [amReminder, setAmReminder] = useState({
@@ -44,7 +47,8 @@ function ToothBrushing() {
 
     
     return (
-        <View>
+        <View style={styles.container}>
+            <Image source={LOGO} style={styles.logo} />
             <Text>Morning Tooth Brushing Reminder</Text>
             <TextInput
                 placeholder="Message"
@@ -54,19 +58,38 @@ function ToothBrushing() {
 
             {/* Add a button to open the time picker for AM reminder */}
             <TouchableOpacity onPress={() => setShowTimePicker(true)}>
-                <Text>Set Time for Reminder</Text>
+                <Text>Set Time for Morning Reminder</Text>
             </TouchableOpacity>
 
             {showTimePicker && (
                 <DateTimePicker
                     value={amReminder.time}
                     mode="time"
-                    is24Hour={true}
+                    is24Hour={false}
+                    display="clock"
+                    onChange={handleTimeChange}
+                    selectedTime={amReminder.time}
+                />
+            )}
+            <Text>Evening Tooth Brushing Reminder</Text>
+            <TextInput
+                placeholder="Message"
+                value={amReminder.message}
+                onChangeText={(text) => setAmReminder({ ...pmReminder, message: text })}
+            />
+            {showTimePicker && (
+                <DateTimePicker
+                    value={pmReminder.time}
+                    mode="time"
+                    is24Hour={false}
                     display="clock"
                     onChange={handleTimeChange}
                 />
             )}
-
+            {/* Add a button to open the time picker for AM reminder */}
+            <TouchableOpacity onPress={() => setShowTimePicker(true)}>
+                <Text>Set Time for Evening Reminder</Text>
+            </TouchableOpacity>
             {/* Add more input fields for additional reminder details as needed */}
             <TouchableOpacity onPress={handleScheduleReminders}>
                 <View
@@ -85,4 +108,44 @@ function ToothBrushing() {
 
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#ffffff',
+        justifyContent: "center", // Center the content vertically
+        alignItems: "center", // Center the content horizontally
+    }, 
+    logo: {
+      width: 100, // Set a width for the logo
+      height: 100, // Set a height for the logo (you can adjust as needed)
+      resizeMode: "contain", // Keep the logo's aspect ratio
+      marginBottom: 20
+    },
+    listItem: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-around', // This already makes sure there's space between items
+      padding: 15,
+      width: '100%', // Use full width of the container 
+    },
+    item: {
+      padding: 10,
+      fontSize: 18,
+      height: 44,
+      width: 200,
+      backgroundColor: "#f7f7f7",
+      color: 'black', 
+      borderWidth: 1,   
+      borderColor: 'black',
+      borderRadius: 25, // Rounded edges
+      marginBottom: 10, // Gap between items
+      textAlign: 'center',
+    },
+    buttonText: {
+      color: 'white',
+      fontSize: 20,
+    },
+
+});
 export default ToothBrushing;
