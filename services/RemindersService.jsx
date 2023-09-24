609af5ef7c3d5bc362d238983a223fa34b44c840
farhayden <haydefa1@student.op.ac.nz>
2023-09-25 00:04:11 +1300

    Modify(function): add second localNotificationSchedule object to be passed to Toothbrushing screen

--- services/RemindersService.jsx
+++ services/RemindersService.jsx
@@ -2,6 +2,14 @@ import PushNotification from 'react-native-push-notification';
 
 function scheduleLocalNotification() {
 
+    const amTime = new Date();
+    amTime.setHours(8); // Set AM hour (e.g., 8 AM)
+    amTime.setMinutes(0); // Set AM minute (e.g., 0 minutes)
+
+    const pmTime = new Date();
+    pmTime.setHours(20); // Set PM hour (e.g., 8 PM)
+    pmTime.setMinutes(0); // Set PM minute (e.g., 0 minutes)
+
     PushNotification.localNotificationSchedule({
         channelId: "BraceMinder-channel-id",
         title: "Tooth Brushing", // (optional)
@@ -11,7 +19,20 @@ function scheduleLocalNotification() {
         vibration: 300,
         subtext: "This is a subtext",
         bigText: "",
-        date: new Date,
+        date: amTime,
+        repeatTime: 'daily',
+    });
+
+    PushNotification.localNotificationSchedule({
+        channelId: "BraceMinder-channel-id",
+        title: "Tooth Brushing", // (optional)
+        message: "My Notification Message", // (required)
+        color: "blue",
+        vibrate: true,
+        vibration: 300,
+        subtext: "This is a subtext",
+        bigText: "",
+        date: pmTime,
         repeatTime: 'daily',
     });
 } export default scheduleLocalNotification;
