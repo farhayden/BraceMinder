dcc423eb8a7ec1c4c9f190bd8d0a1d61399d1498
farhayden <haydefa1@student.op.ac.nz>
2023-09-24 23:59:09 +1300

    Modify(import statement): corrected spelling error of import statement

--- services/Navigation.jsx
+++ services/Navigation.jsx
@@ -4,7 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 
-import HomeScreen from "../screens/HomeScreen";
+import HomeScreen from "../screens/Homescreen";
 import Reminders from "../screens/Reminders";
 import ToothBrushing from "../screens/ToothBrushing";
 
