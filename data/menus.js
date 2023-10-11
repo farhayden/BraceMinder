374d926190740e502881bbdb2a58cb0e3f0fa88b
mojoju <matilda.gill@gmail.com>
2023-10-12 12:57:41 +1300

    add menus (home, how to)

---
+++ data/menus.js
@@ -0,0 +1,52 @@
+export const homeMenu = [
+    {
+      key: "Reminders",
+      discipline: "home",
+    },
+    {
+      key: "My Log",
+      discipline: "home",
+    },
+    {
+      key: "How To",
+      discipline: "home",
+    },
+  ];
+  export const howToMenu = [
+    {
+      key: "Clean your braces",
+      discipline: "howTo",
+      link: "https://drchelian.com/6-tools-for-cleaning-braces/#:~:text=Use%20a%20soft%20toothbrush%20with,bottom%20edges%20of%20the%20brackets.",
+    },
+    {
+      key: "Clean your aligners",
+      discipline: "howTo",
+      link: "https://drstevencasci.co.nz/invisalign-info/how-to-clean-your-aligners/#:~:text=Add%20one%20part%20white%20vinegar,them%20thoroughly%20with%20clean%20water.",
+    },
+    {
+      key: "Clean your retainers",
+      discipline: "howTo",
+      link: "https://www.thebraceplacetulsa.com/dos-and-donts-of-cleaning-your-retainer/#:~:text=Make%20a%20mix%20of%2050,time%20you%20clean%20your%20retainer.",
+    },
+    {
+      key: "Manage breakages",
+      discipline: "howTo",
+      link: "https://www.pureorthodontics.ca/blog/what-to-do-if-you-break-a-bracket#:~:text=Use%20a%20q%2Dtip%20to,with%20you%20to%20your%20appointment.",
+    },
+    {
+      key: "Diet and Food",
+      discipline: "howTo",
+      link: "https://molenorthodontics.com/blog/2016/05/05/healthy-delicious-foods-to-eat-with-braces/#:~:text=Here%20are%20some%20other%20great,soft%2Dcooked%20meats%20like%20chicken",
+    },
+    {
+      key: "Manage tooth pain",
+      discipline: "howTo",
+      link: "https://orthodonticsaustralia.org.au/why-do-your-braces-hurt/",
+    },
+    {
+      key: "Jaw Joint issues",
+      discipline: "howTo",
+      link: "https://my.clevelandclinic.org/health/diseases/15066-temporomandibular-disorders-tmd-overview#:~:text=TMJ%20dysfunction%20(TMD)%20causes%20pain,mouth%20guards%20and%20jaw%20surgery.", 
+    },
+  ];
+  
