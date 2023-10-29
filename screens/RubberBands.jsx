912c6b059e8160890982107f8c6ee86559130820
farhayden <haydefa1@student.op.ac.nz>
2023-10-30 09:27:54 +1300

    Modify(function): declare new function for implementing select date and random reminder functionality

--- screens/RubberBands.jsx
+++ screens/RubberBands.jsx
@@ -4,3 +4,9 @@ import { View, Text, TouchableOpacity, Image, StyleSheet, Button, Picker } from 
 import DateTimePicker from '@react-native-community/datetimepicker';
 import PushNotification from 'react-native-push-notification';
 import logo from "../assets/logo.png";
+
+const LOGO = logo;
+
+function RubberBands() {
+    
+}
