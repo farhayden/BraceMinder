868fb886822e1cbd7de0b387e97ea6a81d39714b
farhayden <haydefa1@student.op.ac.nz>
2023-10-30 11:33:16 +1300

    Modify(return): update return to display icon image and button to navagate to Rubber Bands screen

--- screens/Reminders.jsx
+++ screens/Reminders.jsx
@@ -1,31 +1,43 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, Button, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, Button, StyleSheet, Image } from "react-native";
 import ToothBrushing from "./ToothBrushing";
+import RubberBands from "./RubberBands";
 import { useNavigation } from "@react-navigation/native";
 import scheduleLocalNotification from "../services/RemindersService";
+import logo from "../assets/logo.png";
+
+const LOGO = logo;
 
 function RemindersScreen() {
     const navigation = useNavigation();
   
     const handlePress = () => {
       navigation.navigate('ToothBrushing'); // Navigate to ToothBrushing screen
+      //navigation.navigate('RubberBands'); // Navigate to RubberBands screen
     };
       
-    return (
+    return (<>
+        
         <View style={styles.container}>
-            {/* Other content */}
-            <Button style= {styles.item} title="Tooth Brushing" onPress={handlePress} />
+            <Image source={LOGO} style={styles.logo} />
+            <View>
+                {/* Other content */}
+                <Button style= {styles.item} title="Tooth Brushing" onPress={handlePress} />
+            </View>
+            <View>
+                <Button style= {styles.item} title="Rubber Bands" onPress={handlePress} />
+            </View>
         </View>
-    );
+    </>);
 }
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        padding: 20,
+        flex: 0,
+        height: "100%",
+        flexDirection: "column",
         backgroundColor: '#ffffff',
-        justifyContent: "center", // Center the content vertically
+        justifyContent: "space-around", // Center the content vertically
         alignItems: "center", // Center the content horizontally
-        
     },
     logo: {
         width: 100, // Set a width for the logo
@@ -33,19 +45,14 @@ const styles = StyleSheet.create({
         resizeMode: "contain", // Keep the logo's aspect ratio
         marginBottom: 20
     },
-    item: {
-        padding: 10,
-        fontSize: 18,
-        height: 44,
-        width: 200,
-        backgroundColor: "#f7f7f7",
-        color: 'black', 
-        borderWidth: 1,   
-        borderColor: 'black',
-        borderRadius: 25, // Rounded edges
-        marginBottom: 10, // Gap between items
-        textAlign: 'center',
     
+    button: {
+        borderRadius: 15,
+        color: 'white',
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 20,
     },
 }); 
 export default RemindersScreen;
