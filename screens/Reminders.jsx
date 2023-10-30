af4a8a85cafc9e4f73a85a763a87567d86c1e712
farhayden <haydefa1@student.op.ac.nz>
2023-10-30 14:49:23 +1300

    Modify(list,switches): add list and switches for navigation to the notification screens

--- screens/Reminders.jsx
+++ screens/Reminders.jsx
@@ -1,32 +1,50 @@
 import React, { useState } from "react";
-import { View, Text, TouchableOpacity, Button, StyleSheet, Image } from "react-native";
+import { View, Text, TouchableOpacity, Button, StyleSheet, Image, FlatList, Switch } from "react-native";
 import ToothBrushing from "./ToothBrushing";
 import RubberBands from "./RubberBands";
 import { useNavigation } from "@react-navigation/native";
 import scheduleLocalNotification from "../services/RemindersService";
 import logo from "../assets/logo.png";
+import tasks from "../data/tasks";
 
 const LOGO = logo;
 
 function RemindersScreen() {
     const navigation = useNavigation();
+
+    const [taskSwitches, setTaskSwitches] = useState({});
+
+    const handleSwitch = (taskId, value) => {
+        setTaskSwitches(prevSwitches => ({
+          ...prevSwitches,
+          [taskId]: value // Using the task ID as the key to store each task's switch state
+        }));
+    };
   
-    const handlePress = () => {
-      navigation.navigate('ToothBrushing'); // Navigate to ToothBrushing screen
-      //navigation.navigate('RubberBands'); // Navigate to RubberBands screen
+    const handlePress = (screenName) => {
+        navigation.navigate(screenName); // Navigate to the screen with the specified screenName
     };
       
     return (<>
         
         <View style={styles.container}>
             <Image source={LOGO} style={styles.logo} />
-            <View>
-                {/* Other content */}
-                <Button style= {styles.item} title="Tooth Brushing" onPress={handlePress} />
-            </View>
-            <View>
-                <Button style= {styles.item} title="Rubber Bands" onPress={handlePress} />
-            </View>
+            <FlatList
+                data={tasks}
+                renderItem={({ item }) => (
+                    <TouchableOpacity onPress={() => handlePress(item.key)} style={styles.listItem}>
+                        <Text style={styles.item}>{item.key}</Text>
+                        <Switch
+                            style={{ transform: [{ scaleX: 1.5 }, { scaleY: 1.5 }] }}
+                            trackColor={{ false: "#ffa500", true: "#50C878" }}
+                            thumbColor={taskSwitches[item.id] ? "#ffffff" : "#fffffff"}
+                            ios_backgroundColor="#3e3e3e"
+                            onValueChange={(value) => handleSwitch(item.id, value)}
+                            value={taskSwitches[item.id] || false}  // This line makes sure each switch has its own unique state
+                        />
+                    </TouchableOpacity>
+                )}
+            />
         </View>
     </>);
 }
@@ -53,6 +71,28 @@ const styles = StyleSheet.create({
     buttonText: {
         color: 'white',
         fontSize: 20,
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+        width: 200,
+        backgroundColor: "#f7f7f7",
+        color: 'black', 
+        borderWidth: 1,   
+        borderColor: 'black',
+        borderRadius: 25, // Rounded edges
+        marginBottom: 10, // Gap between items
+        textAlign: 'center',
+    
+    },
+
+    listItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around', // This already makes sure there's space between items
+        padding: 5,
+        width: '100%', // Use full width of the container
     },
 }); 
 export default RemindersScreen;
